16928a635b3718b6df8b471432cab7d9dff3fcd9
anujtalan95 <anuj25talan@gmail.com>
2017-03-10 19:02:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#E-commerce mobile responsive website with SEO features
+
+####An Online Retail Management and Customer Order Processing System
+
+####-By Anuj Kumar Talan
+
+##Features
+
+* An online retail website system using java technologies
+* Frontend supported with HTML5, CSS3 and JQuery
---
+++ assets/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.product-carousel').slick({
+    infinite:false,
+    arrows:true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows:false
+        }
+      }
+    ]
+  });
+});
